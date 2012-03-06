5de72390ed82da8ef6db79a0e72c64479f2beb1e
Zach Williams <hey@zachwill.com>
2012-03-05 16:25:44 -0800

    Add SMS processing tests

--- test.py
+++ test.py
@@ -3,7 +3,11 @@
 """Tests for the Flask Heroku template."""
 
 import unittest
+from mock import Mock
+
+import sms
 from app import app
+from sms import process
 
 
 class TestApp(unittest.TestCase):
@@ -16,14 +20,9 @@ class TestApp(unittest.TestCase):
         self.assertTrue(rv.data)
         self.assertEquals(rv.status_code, 200)
 
-    def test_about_page_works(self):
-        rv = self.app.get('/about/')
-        self.assertTrue(rv.data)
-        self.assertEquals(rv.status_code, 200)
-
     def test_default_redirecting(self):
-        rv = self.app.get('/about')
-        self.assertEquals(rv.status_code, 301)
+        rv = self.app.get('/issue')
+        self.assertEquals(rv.status_code, 302)
 
     def test_404_page(self):
         rv = self.app.get('/i-am-not-found/')
@@ -33,6 +32,22 @@ class TestApp(unittest.TestCase):
         rv = self.app.get('/robots.txt')
         self.assertTrue(rv.data)
         self.assertEquals(rv.status_code, 200)
+
+
+class ProcessTextMessage(unittest.TestCase):
+
+    def setUp(self):
+        sms.respond = Mock()
+        sms.seeclickfix = Mock()
+
+    def test_against_a_fake_text_message(self):
+        text = {
+            'senderAddress': 'tel:+478-555-5555',
+            'message': 'This is a test.'
+        }
+        process(text)
+        sms.respond.assert_called_with('478-555-5555')
+        sms.seeclickfix.assert_called_with('This is a test.')
 
 
 if __name__ == '__main__':
