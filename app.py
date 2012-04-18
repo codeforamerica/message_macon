eb1d5f130293b1990d8c8b63d61a64f2c30607d3
Zach Williams <hey@zachwill.com>
2012-04-18 12:34:37 -0700

    The website should be hosted elsewhere. This should only be for text messages.

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ This file creates your application.
 
 import os
 from simplejson import loads
-from flask import Flask, render_template, request, redirect
 
+from flask import Flask, render_template, request, redirect
 from sms import process
 
 app = Flask(__name__)
+
 
 if 'SECRET_KEY' in os.environ:
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -20,17 +21,7 @@ else:
     app.config['SECRET_KEY'] = 'this_should_be_configured'
 
 
-###
-# Routing for your application.
-###
-
-@app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
-
-
-@app.route('/issue', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def issue():
     """Receive an incoming SMS message issue."""
     if request.method == 'POST':
@@ -42,35 +33,7 @@ def issue():
         finally:
             process(text)
             return 'Success!'
-    return redirect('/')
-
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
+    return "Not a POST request."
 
 
 if __name__ == '__main__':
