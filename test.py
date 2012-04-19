f630675b108d14896930e00ec8b6c5583c599ae0
Zach Williams <hey@zachwill.com>
2012-04-18 17:58:06 -0700

    Remove useless tests

--- test.py
+++ test.py
@@ -6,28 +6,7 @@ import unittest
 from mock import Mock
 
 import sms
-from app import app
 from sms import process, find_address, AddressError
-
-
-class TestApp(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_home_page_works(self):
-        rv = self.app.get('/')
-        self.assertTrue(rv.data)
-        self.assertEquals(rv.status_code, 200)
-
-    def test_default_redirecting(self):
-        rv = self.app.get('/issue')
-        self.assertEquals(rv.status_code, 302)
-
-    def test_static_text_file_request(self):
-        rv = self.app.get('/robots.txt')
-        self.assertTrue(rv.data)
-        self.assertEquals(rv.status_code, 200)
 
 
 class ProcessTextMessage(unittest.TestCase):
